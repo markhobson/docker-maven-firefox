6ebfdd86f08a09472cbbe5dc16ecb6ac4a63e6bf
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2021-05-03 20:28:57 +0200

    --rm remove container after exists

--- demo/run-jjcanada.sh
+++ demo/run-jjcanada.sh
@@ -3,4 +3,4 @@
 DOCKER_IMAGE=ualjjcanada/maven-firefox
 
 docker pull $DOCKER_IMAGE
-docker run -it -v "$HOME/.m2":/root/.m2 -v "$PWD":/usr/src -w /usr/src $DOCKER_IMAGE mvn clean verify
+docker run --rm -it -v "$HOME/.m2":/root/.m2 -v "$PWD":/usr/src -w /usr/src $DOCKER_IMAGE mvn clean verify
